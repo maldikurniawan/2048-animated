90adf399a74ed120063fc5c1bd80f0fa241e00fc
maldikurniawan <akurniawan2102@gmail.com>
2024-07-23 13:18:45 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./main.scss"
+import "./styles.scss"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <h1>
+    2048
+  </h1>
+)
